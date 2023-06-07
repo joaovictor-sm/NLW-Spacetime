78ec3f801e12486782061f0fb7c28307072a0ea6
jvsmscript <dbase913@gmail.com>
2023-06-07 17:49:38 -0300

    Front-end created and Routes created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true, // Option access server ==> ['https://localhost:3000']
+})
+
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on https://localhost:3333')
+    console.log('🚀 HTTP server is running on http://localhost:3333')
   })
